6e54a12987ada54a6e0d6261f12596ce053563dd
Bob Tang <254202042@qq.com>
2022-03-18 11:24:41 +0800

    update:发现路由的匹配规则

--- README.md
+++ README.md
@@ -8,3 +8,40 @@
 ```jsx
 import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom'
 ```
+
+
+
+路由设置的注意点：
+
+```jsx
+ {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+```
+
+当你如下定义路由的时候，都会命中 `/`
+
+```jsx
+<Switch>
+  <Route path="/">
+    <Home />
+  </Route>
+  <Route path="/users">
+    <Users />
+  </Route>
+  <Route path="/about">
+    <About />
+  </Route>
+</Switch>
+```
+
+
+
+1. `/`
+
+2. `/users`
+
+3. `/about`
+
+也就是说路由的在Switch中的顺序很重要,他会遍历其子元素，匹配到就停止了。当你跳转到`/home`或者`/user`的时候，会命中`/`。 出现如下的效果：
+
+![image-20220318111753957](./src/img/image-20220318111753957.png)
--- src/pages/BasicRoute.js
+++ src/pages/BasicRoute.js
@@ -1,1 +1,64 @@
 import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+
+function BasicRoute() {
+    return(
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">User</Link>
+                        </li>
+                    </ul>
+                </nav>
+                {/* 
+                Switch looks through its children Route
+                and renders the first that match the url
+                 */}
+                 <Switch>
+                     <Route path="/">
+                         <Home />
+                     </Route>
+                     <Route path="/about">
+                         <About />
+                     </Route>
+                     <Route path="/users">
+                        <Users />
+                     </Route>
+                 </Switch>
+            </div>
+        </Router>
+    )
+}
+
+function About() {
+    return(
+        <h2>About</h2>
+    )
+}
+
+function Users() {
+    return(
+        <h2>Users</h2>
+    )
+}
+
+function Home() {
+    return(
+        <h2>Home</h2>
+    )
+}
+export default BasicRoute;
